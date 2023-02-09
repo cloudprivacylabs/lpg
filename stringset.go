4be67a7a03297feb16e3b749776a0e5823357407
Burak Serdar <brxd@cloudprivacylabs.com>
2023-02-09 12:49:50 -0700

    AddSet overwrote the set

--- stringset.go
+++ stringset.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -95,7 +95,7 @@ func (set *StringSet) Add(s ...string) *StringSet {
 func (set *StringSet) AddSet(s StringSet) *StringSet {
 	m := set.M
 	set.M = make(map[string]struct{}, len(s.M)+len(m))
-	for x := range s.M {
+	for x := range m {
 		set.M[x] = struct{}{}
 	}
 	for x := range s.M {
