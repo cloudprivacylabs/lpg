9e643111f57e78e58883a4595a71be5681a154ae
Jason Cruz <jac024@ucsd.edu>
2023-02-24 08:30:12 -0800

    benchmarks

--- bench_test.go
+++ bench_test.go
@@ -2,7 +2,6 @@ package lpg
 
 import (
 	"fmt"
-	"math/rand"
 	"testing"
 )
 
@@ -10,21 +9,15 @@ func BenchmarkPropNonExistsGraph(b *testing.B) {
 	g := NewGraph()
 	nodes := make([]*Node, 0)
 	for i := 0; i < 1000; i++ {
-		nodes = append(nodes, g.NewNode([]string{fmt.Sprint(i)}, nil))
+		nodes = append(nodes, g.NewNode([]string{fmt.Sprint(i)}, map[string]interface{}{"a": "b", "c": "d", "e": "f", "g": "h"}))
 	}
 	labels := []string{"a", "b", "c", "d"}
 	for i := 0; i < len(nodes)-1; i++ {
 		g.NewEdge(nodes[i], nodes[i+1], labels[i%4], nil)
 	}
-	hasProp := func(node *Node) bool {
-		return node.ForEachProperty(func(s string, i interface{}) bool {
-			_, ok := node.GetProperty(s)
-			return ok
-		})
-	}
 	for n := 0; n < b.N; n++ {
 		for nodes := g.GetNodes(); nodes.Next(); {
-			hasProp(nodes.Node())
+			nodes.Node().GetProperty("z")
 		}
 	}
 }
@@ -32,26 +25,20 @@ func BenchmarkPropNonExistsGraph(b *testing.B) {
 func BenchmarkPropExistsGraph(b *testing.B) {
 	g := NewGraph()
 	nodes := make([]*Node, 0)
-	for i := 0; i < 999; i++ {
-		nodes = append(nodes, g.NewNode([]string{fmt.Sprint(i)}, nil))
+	for i := 0; i < 1000; i++ {
+		if i < 500 {
+			nodes = append(nodes, g.NewNode([]string{fmt.Sprint(i)}, map[string]interface{}{"a": "b", "c": "d", "e": "f", "g": "h"}))
+		} else {
+			nodes = append(nodes, g.NewNode([]string{fmt.Sprint(i)}, map[string]interface{}{"a": "b", "c": "d", "e": "f", "g": "h", "z": "zz"}))
+		}
 	}
-	nodes = append(nodes, g.NewNode([]string{fmt.Sprint(999)}, map[string]interface{}{"a": "b", "c": "d", "e": "f", "g": "h"}))
-	s := nodes[len(nodes)-1]
-	nodes[len(nodes)-1] = nodes[len(nodes)/2]
-	nodes[len(nodes)/2] = s
 	labels := []string{"a", "b", "c", "d"}
 	for i := 0; i < len(nodes)-1; i++ {
 		g.NewEdge(nodes[i], nodes[i+1], labels[i%4], nil)
 	}
-	hasProp := func(node *Node) bool {
-		return node.ForEachProperty(func(s string, i interface{}) bool {
-			_, ok := node.GetProperty(s)
-			return ok
-		})
-	}
 	for n := 0; n < b.N; n++ {
 		for nodes := g.GetNodes(); nodes.Next(); {
-			hasProp(nodes.Node())
+			nodes.Node().GetProperty("z")
 		}
 	}
 }
@@ -69,14 +56,17 @@ func BenchmarkDeleteEdge(b *testing.B) {
 		}
 	}
 	for n := 0; n < b.N; n++ {
-		nc := 0
-		rn := rand.Intn(100)
 		for nodes := g.GetNodes(); nodes.Next(); {
-			nc++
 			node := nodes.Node()
-			for edges := node.GetEdges(OutgoingEdge); edges.Next(); {
-				if nc == rn {
-					g.disconnect(edges.Edge())
+			for {
+				edgeRemoved := false
+				for edges := node.GetEdges(OutgoingEdge); edges.Next(); {
+					edges.Edge().Remove()
+					edgeRemoved = true
+					break
+				}
+				if !edgeRemoved {
+					break
 				}
 			}
 		}
@@ -102,7 +92,7 @@ func BenchmarkFindEdgeLabel(b *testing.B) {
 		for nodes := g.GetNodes(); nodes.Next(); {
 			node := nodes.Node()
 			for edges := node.GetEdges(OutgoingEdge); edges.Next(); {
-				edgeHasLabel(edges.Edge(), labels[rand.Intn(len(labels))])
+				edgeHasLabel(edges.Edge(), "h")
 			}
 		}
 	}
@@ -117,24 +107,18 @@ func BenchmarkFindEdgeProp(b *testing.B) {
 	labels := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i"}
 	for i := 0; i < len(nodes)-1; i++ {
 		for _, label := range labels {
-			if i == rand.Intn(len(nodes)) {
-				g.NewEdge(nodes[i], nodes[i+1], label, map[string]interface{}{"a": "b", "c": "d", "e": "f", "g": "h"})
+			if i < len(nodes)/2 {
+				g.NewEdge(nodes[i], nodes[i+1], label, map[string]interface{}{"a": "b", "c": "d", "e": "f", "g": "h", "z": "zz"})
 			} else {
 				g.NewEdge(nodes[i], nodes[i+1], label, nil)
 			}
 		}
 	}
-	edgeHasProp := func(edge *Edge) bool {
-		return edge.ForEachProperty(func(s string, i interface{}) bool {
-			_, ok := edge.GetProperty(s)
-			return ok
-		})
-	}
 	for n := 0; n < b.N; n++ {
 		for nodes := g.GetNodes(); nodes.Next(); {
 			node := nodes.Node()
 			for edges := node.GetEdges(OutgoingEdge); edges.Next(); {
-				edgeHasProp(edges.Edge())
+				edges.Edge().GetProperty("z")
 			}
 		}
 	}
--- pattern_test.go
+++ pattern_test.go
@@ -11,7 +11,7 @@ func TestPattern(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
 	}
-	for i := 0; i < 8; i++ {
+	for i := 0; i < 9; i++ {
 		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
 	}
 	nodes[5].SetProperty("key", "value")
@@ -78,7 +78,7 @@ func TestLoopPattern(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
 	}
-	for i := 0; i < 8; i++ {
+	for i := 0; i < 9; i++ {
 		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
 	}
 	symbols := make(map[string]*PatternSymbol)
@@ -119,7 +119,7 @@ func TestVariableLengthPath(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
 	}
-	for i := 0; i < 8; i++ {
+	for i := 0; i < 9; i++ {
 		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
 	}
 
@@ -156,4 +156,281 @@ func TestVariableLengthPath(t *testing.T) {
 		t.Errorf("Expecting 3 nodes: %+v", out)
 	}
 
+}
+
+func TestSimpleDirectedPathPatternWithIndex(t *testing.T) {
+	testSimpleDirectedPathPattern(t, true)
+}
+func TestSimpleDirectedPathPatternWithoutIndex(t *testing.T) {
+	testSimpleDirectedPathPattern(t, false)
+}
+
+// (n:label) -[*]-> (m:label)
+func testSimpleDirectedPathPattern(t *testing.T, withIndex bool) {
+	graph := NewGraph()
+	if withIndex {
+		graph.index.NodePropertyIndex("key", graph, BtreeIndex)
+	}
+	nodes := make([]*Node, 0)
+	for i := 0; i < 10; i++ {
+		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
+	}
+	for i := 0; i < 9; i++ {
+		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
+	}
+	nodes[5].SetProperty("key", "value")
+	nodes[6].SetProperty("key", "value")
+	pat := Pattern{
+		{Name: "n", Labels: StringSet{}},
+		{Min: 1, Max: 1},
+		{Name: "n2", Labels: StringSet{}}}
+	symbols := make(map[string]*PatternSymbol)
+	acc := &DefaultMatchAccumulator{}
+	if err := pat.Run(graph, symbols, acc); err != nil {
+		t.Error(err)
+		return
+	}
+	if acc.Paths[5].([]*Edge)[0].GetFrom() != nodes[5] {
+		t.Errorf("Expecting %v, got: %v", acc.Paths[5].([]*Edge)[0].GetFrom(), nodes[5])
+	}
+}
+
+func TestSimplePathPatternPatternWithIndex(t *testing.T) {
+	testSimplePathPattern(t, true)
+}
+
+func TestSimplePathPatternWithoutIndex(t *testing.T) {
+	testSimplePathPattern(t, false)
+}
+
+// (n:label)-[]-(m:label)
+func testSimplePathPattern(t *testing.T, withIndex bool) {
+	graph := NewGraph()
+	if withIndex {
+		graph.index.NodePropertyIndex("key", graph, BtreeIndex)
+	}
+	nodes := make([]*Node, 0)
+	for i := 0; i < 10; i++ {
+		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
+	}
+	for i := 0; i < 9; i++ {
+		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
+	}
+	nodes[2].SetProperty("key", "value")
+	nodes[8].SetProperty("key", "value")
+	pat := Pattern{
+		{Name: "n", Labels: StringSet{}},
+		{Min: -1, Max: 1, Undirected: true},
+		{Name: "n2", Labels: StringSet{}}}
+	symbols := make(map[string]*PatternSymbol)
+	acc := &DefaultMatchAccumulator{}
+	if err := pat.Run(graph, symbols, acc); err != nil {
+		t.Error(err)
+		return
+	}
+	if acc.Paths[4].([]*Edge)[0].GetFrom() != nodes[2] {
+		t.Errorf("Expecting %v, got: %v", acc.Paths[9].([]*Edge)[0].GetFrom(), nodes[2])
+	}
+}
+
+func TestVariablePathPatternWithIndex(t *testing.T) {
+	testVariablePathPattern(t, true)
+}
+
+func TestVariablePathPatternWithoutIndex(t *testing.T) {
+	testVariablePathPattern(t, false)
+}
+
+// (n:label)-[*]-(m:label)
+func testVariablePathPattern(t *testing.T, withIndex bool) {
+	graph := NewGraph()
+	if withIndex {
+		graph.index.NodePropertyIndex("key", graph, BtreeIndex)
+	}
+	nodes := make([]*Node, 0)
+	for i := 0; i < 10; i++ {
+		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
+	}
+	for i := 0; i < 9; i++ {
+		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
+	}
+	nodes[2].SetProperty("key", "value")
+	nodes[8].SetProperty("key", "value")
+	pat := Pattern{
+		{Name: "n", Labels: StringSet{}},
+		{Min: -1, Max: -1, Undirected: true},
+		{Name: "n2", Labels: StringSet{}}}
+	symbols := make(map[string]*PatternSymbol)
+	acc := &DefaultMatchAccumulator{}
+	if err := pat.Run(graph, symbols, acc); err != nil {
+		t.Error(err)
+		return
+	}
+	if acc.Paths[179].([]*Edge)[0].GetFrom() != nodes[8] {
+		t.Errorf("Expecting %v, got: %v", acc.Paths[9].([]*Edge)[0].GetFrom(), nodes[2])
+	}
+}
+
+func TestPathLengthTwoPatternWithIndex(t *testing.T) {
+	testPathLengthTwoPattern(t, true)
+}
+
+func TestPathLengthTwoPatternWithoutIndex(t *testing.T) {
+	testPathLengthTwoPattern(t, false)
+}
+
+func testPathLengthTwoPattern(t *testing.T, withIndex bool) {
+	graph := NewGraph()
+	if withIndex {
+		graph.index.NodePropertyIndex("key", graph, BtreeIndex)
+	}
+	nodes := make([]*Node, 0)
+	for i := 0; i < 10; i++ {
+		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
+	}
+	for i := 0; i < 9; i++ {
+		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
+	}
+	nodes[4].SetProperty("key", "value")
+	nodes[7].SetProperty("key", "value")
+	pat := Pattern{
+		{Name: "n", Labels: StringSet{}},
+		{Min: 2, Max: 2, Undirected: true},
+		{Name: "n2", Labels: StringSet{}}}
+	symbols := make(map[string]*PatternSymbol)
+	acc := &DefaultMatchAccumulator{}
+	if err := pat.Run(graph, symbols, acc); err != nil {
+		t.Error(err)
+		return
+	}
+	if acc.Paths[27].([]*Edge)[0].GetFrom() != nodes[7] {
+		t.Errorf("Expecting %v, got: %v", acc.Paths[15].([]*Edge)[0].GetFrom(), nodes[7])
+	}
+}
+
+func benchmarkSimpleDirectedPathPattern(b *testing.B, withIndex bool) {
+	graph := NewGraph()
+	if withIndex {
+		graph.index.NodePropertyIndex("key", graph, BtreeIndex)
+	}
+	nodes := make([]*Node, 0)
+	for i := 0; i < 10; i++ {
+		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
+	}
+	for i := 0; i < 9; i++ {
+		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
+	}
+	nodes[5].SetProperty("key", "value")
+	nodes[6].SetProperty("key", "value")
+	pat := Pattern{
+		{Name: "n", Labels: StringSet{}},
+		{Min: 1, Max: 1},
+		{Name: "n2", Labels: StringSet{}}}
+	symbols := make(map[string]*PatternSymbol)
+	acc := &DefaultMatchAccumulator{}
+	for n := 0; n < b.N; n++ {
+		pat.Run(graph, symbols, acc)
+	}
+}
+
+func BenchmarkSimpleDirectedPathPatternWithIndex(b *testing.B) {
+	benchmarkSimpleDirectedPathPattern(b, true)
+}
+func BenchmarkSimpleDirectedPathPatternWithoutIndex(b *testing.B) {
+	benchmarkSimpleDirectedPathPattern(b, false)
+}
+
+func benchmarkSimplePathPattern(b *testing.B, withIndex bool) {
+	graph := NewGraph()
+	if withIndex {
+		graph.index.NodePropertyIndex("key", graph, BtreeIndex)
+	}
+	nodes := make([]*Node, 0)
+	for i := 0; i < 10; i++ {
+		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
+	}
+	for i := 0; i < 9; i++ {
+		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
+	}
+	nodes[2].SetProperty("key", "value")
+	nodes[8].SetProperty("key", "value")
+	pat := Pattern{
+		{Name: "n", Labels: StringSet{}},
+		{Min: -1, Max: 1, Undirected: true},
+		{Name: "n2", Labels: StringSet{}}}
+	symbols := make(map[string]*PatternSymbol)
+	acc := &DefaultMatchAccumulator{}
+	for n := 0; n < b.N; n++ {
+		pat.Run(graph, symbols, acc)
+	}
+}
+
+func BenchmarkSimplePathPatternWithIndex(b *testing.B) {
+	benchmarkSimplePathPattern(b, true)
+}
+func BenchmarkSimplePathPatternWithoutIndex(b *testing.B) {
+	benchmarkSimplePathPattern(b, false)
+}
+
+func benchmarkVariablePathPattern(b *testing.B, withIndex bool) {
+	graph := NewGraph()
+	if withIndex {
+		graph.index.NodePropertyIndex("key", graph, BtreeIndex)
+	}
+	nodes := make([]*Node, 0)
+	for i := 0; i < 10; i++ {
+		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
+	}
+	for i := 0; i < 9; i++ {
+		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
+	}
+	nodes[2].SetProperty("key", "value")
+	nodes[8].SetProperty("key", "value")
+	pat := Pattern{
+		{Name: "n", Labels: StringSet{}},
+		{Min: -1, Max: -1, Undirected: true},
+		{Name: "n2", Labels: StringSet{}}}
+	symbols := make(map[string]*PatternSymbol)
+	acc := &DefaultMatchAccumulator{}
+	for n := 0; n < b.N; n++ {
+		pat.Run(graph, symbols, acc)
+	}
+}
+
+func BenchmarkVariablePathPatternWithIndex(b *testing.B) {
+	benchmarkVariablePathPattern(b, true)
+}
+func BenchmarkVariablePathPatternWithoutIndex(b *testing.B) {
+	benchmarkVariablePathPattern(b, false)
+}
+
+func benchmarkPathLengthTwoPattern(b *testing.B, withIndex bool) {
+	graph := NewGraph()
+	if withIndex {
+		graph.index.NodePropertyIndex("key", graph, BtreeIndex)
+	}
+	nodes := make([]*Node, 0)
+	for i := 0; i < 10; i++ {
+		nodes = append(nodes, graph.NewNode([]string{"a"}, nil))
+	}
+	for i := 0; i < 9; i++ {
+		graph.NewEdge(nodes[i], nodes[i+1], "label", nil)
+	}
+	nodes[4].SetProperty("key", "value")
+	nodes[7].SetProperty("key", "value")
+	pat := Pattern{
+		{Name: "n", Labels: StringSet{}},
+		{Min: 2, Max: 2, Undirected: true},
+		{Name: "n2", Labels: StringSet{}}}
+	symbols := make(map[string]*PatternSymbol)
+	acc := &DefaultMatchAccumulator{}
+	for n := 0; n < b.N; n++ {
+		pat.Run(graph, symbols, acc)
+	}
+}
+func BenchmarkPathLengthTwoPatternWithIndex(b *testing.B) {
+	benchmarkPathLengthTwoPattern(b, true)
+}
+func BenchmarkPathLengthTwoPatternWithoutIndex(b *testing.B) {
+	benchmarkPathLengthTwoPattern(b, false)
 }
